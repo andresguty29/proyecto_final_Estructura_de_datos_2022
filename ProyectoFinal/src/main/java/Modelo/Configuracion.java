282dd5b3a06c0a295ecbd022110095f3555f629e
AFallas-23 <allan232001@gmail.com>
2022-12-16 21:39:31 -0600

    Excel

--- ProyectoFinal/src/main/java/Modelo/Configuracion.java
+++ ProyectoFinal/src/main/java/Modelo/Configuracion.java
@@ -4,10 +4,10 @@ public class Configuracion {
     //C:\Users\User\Documents\NetBeansProjects\GITHUB\proyecto_final_Estructura_de_datos_2022
     //C:\\Users\\roberto.cervantes\\Documents\\NetBeansProjects\\proyecto_final_Estructura_de_datos_2022-main\\ProyectoFinal\\
     //C:\Users\Extremetech\Documents\GitHub\Estructura_de_Datos\proyecto_final_Estructura_de_datos_2022\ProyectoFinal
-
-
-// C:\\Users\\andre\\Documents\\ValeryLocal\\NetBeansProjects\\proyecto_final_Estructura_de_datos_2022\\ProyectoFinal\\
-    String carpeta = "C:\\Users\\andre\\Documents\\ValeryLocal\\NetBeansProjects\\proyecto_final_Estructura_de_datos_2022\\ProyectoFinal\\";
+    //C:\\Users\\XPC\\Documents\\GitHub\\proyecto_final_Estructura_de_datos_2022\\ProyectoFinal\\
+    //C:\\Users\\andre\\Documents\\ValeryLocal\\NetBeansProjects\\proyecto_final_Estructura_de_datos_2022\\ProyectoFinal\\
+    
+    String carpeta = "C:\\Users\\XPC\\Documents\\GitHub\\proyecto_final_Estructura_de_datos_2022\\ProyectoFinal\\";
     String archivo = "Datos.xlsx";
 
     public Configuracion() {
